ff1dbf82e60a9da32e13ecab7b5229650916c20d
adayoldbagel <a.day.0ld.bag3l@gmail.com>
2024-09-11 17:39:47 -0600

    zmanifold: Exposed ManifoldVec and Manifold.BatchBoolean

--- src/zmanifold.zig
+++ src/zmanifold.zig
@@ -60,6 +60,12 @@ pub const Manifold = opaque {
         return @as(*Manifold, @ptrCast(c.manifold_boolean(mem.ptr, first, second, @intFromEnum(operation))));
     }
 
+    pub fn batchBoolean(alloc: Alloc, vec: *ManifoldVec, operation: BooleanOperation) !*Manifold {
+        const mem = try alloc.alloc(u8, c.manifold_manifold_size());
+        const c_vec = @as(?*c.ManifoldManifoldVec, @ptrCast(vec));
+        return @as(*Manifold, @ptrCast(c.manifold_batch_boolean(mem.ptr, c_vec, @intFromEnum(operation))));
+    }
+
     pub fn trimByPlane(self: *Manifold, alloc: Alloc, nx: f64, ny: f64, nz: f64, offset: f64) !*Manifold {
         const mem = try alloc.alloc(u8, c.manifold_manifold_size());
         const original = @as(?*c.ManifoldManifold, @ptrCast(self));
@@ -107,6 +113,59 @@ pub const Manifold = opaque {
 
     pub fn getNumVerts(self: *Manifold) i32 {
         return c.manifold_num_vert(@as(?*c.ManifoldManifold, @ptrCast(self)));
+    }
+};
+
+//----------------------------------------------------------------------------------------------------------
+//
+// ManifoldVec
+//
+//----------------------------------------------------------------------------------------------------------
+
+pub const ManifoldVec = opaque {
+    pub fn initEmpty(alloc: Alloc) !*ManifoldVec {
+        const mem = try alloc.alloc(u8, c.manifold_manifold_vec_size());
+        return @as(*ManifoldVec, @ptrCast(c.manifold_manifold_empty_vec(mem.ptr)));
+    }
+
+    pub fn initSize(alloc: Alloc, size: usize) !*ManifoldVec {
+        const mem = try alloc.alloc(u8, c.manifold_manifold_vec_size());
+        return @as(*ManifoldVec, @ptrCast(c.manifold_manifold_vec(mem.ptr, size)));
+    }
+
+    pub fn deinit(self: *ManifoldVec, alloc: Alloc) void {
+        c.manifold_destruct_manifold_vec(@as(?*c.ManifoldManifoldVec, @ptrCast(self)));
+        const many_ptr = @as([*]u8, @ptrCast(self));
+        alloc.free(many_ptr[0..c.manifold_manifold_vec_size()]);
+    }
+
+    pub fn reserve(self: *ManifoldVec, size: usize) void {
+        c.manifold_manifold_vec_reserve(@as(?*c.ManifoldManifoldVec, @ptrCast(self)), size);
+    }
+
+    pub fn len(self: *ManifoldVec) usize {
+        return c.manifold_manifold_vec_length(@as(?*c.ManifoldManifoldVec, @ptrCast(self)));
+    }
+
+    pub fn get(self: *ManifoldVec, alloc: Alloc, index: i32) !*Manifold {
+        const mem = try alloc.alloc(u8, c.manifold_manifold_size());
+        const mani = c.manifold_manifold_vec_get(mem.ptr, @as(?*c.ManifoldManifoldVec, @ptrCast(self)), index);
+        return @as(*Manifold, @ptrCast(mani));
+    }
+
+    pub fn set(self: *ManifoldVec, index: i32, manifold: *Manifold) void {
+        c.manifold_manifold_vec_set(
+            @as(?*c.ManifoldManifoldVec, @ptrCast(self)),
+            index,
+            @as(*c.ManifoldManifold, @ptrCast(manifold)),
+        );
+    }
+
+    pub fn pushBack(self: *ManifoldVec, manifold: *Manifold) void {
+        c.manifold_manifold_vec_push_back(
+            @as(?*c.ManifoldManifoldVec, @ptrCast(self)),
+            @as(*c.ManifoldManifold, @ptrCast(manifold)),
+        );
     }
 };
 
@@ -175,7 +234,7 @@ pub const Polygons = opaque {
 
     pub fn getPoint(self: *Polygons, simple_idx: i32, point_idx: i32) [2]f64 {
         const vec2 = c.manifold_polygons_get_point(@as(?*c.ManifoldPolygons, @ptrCast(self)), simple_idx, point_idx);
-        return .{vec2.x, vec2.y};
+        return .{ vec2.x, vec2.y };
     }
 };
 
@@ -186,7 +245,6 @@ pub const Polygons = opaque {
 //----------------------------------------------------------------------------------------------------------
 
 pub const CrossSection = opaque {
-
     pub const FillRule = enum {
         even_odd,
         non_zero,
